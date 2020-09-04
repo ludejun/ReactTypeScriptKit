70bb5e65952f477bd5cf0b93218a533d6cc73bd9
ludejun <ludejun@meituan.com>
2020-09-04 11:20:02 +0800

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { App } from './routes/App';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("app")
+);
---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+export default class App extends React.Component {
+  render () {
+
+  }
+}
