14ea96a60be3487ff2f22c37862eeccb30292be1
ludejun <david.lu@loyalvalleycapital.com>
2021-01-21 14:42:13 +0800

    添加ts配置和示例

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './routes/App';
+import App from './routes/App';
+import { Hello } from './routes/Hello';
+
+// const Hello = (props) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+console.log(App);
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById("app")
+  <App />,
+  document.getElementById("main")
 );
+
+// // Webpack Hot Module Replacement API
+// if (module.hot) {
+//   module.hot.accept('./routes/Hello.tsx', () => render(Hello));
+// }
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -4,9 +4,13 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { Hello } from './Hello';
 
-export default class App extends React.Component {
+
+export default class App extends React.Component<{}, {}> {
   render () {
-
+    return (
+      <Hello compiler = 'TypeScript' framework = 'React' />
+    )
   }
 }
